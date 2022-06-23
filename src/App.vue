74af92fea1dbee76903bdab364fe9f9a7172ac7e
ChaiYihan <2366791707@qq.com>
2022-06-23 23:20:56 +0800

    vedio 1 31:15

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Header></Header>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 nav {
-  padding: 30px;
+  padding: 0px;
 }
 
 nav a {
@@ -28,3 +20,14 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+import Header from "@/components/Header"
+
+export default{
+  name: "Layout",
+  components: {
+    Header
+  }
+}
+</script>
