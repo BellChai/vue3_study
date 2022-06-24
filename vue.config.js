e4f769d09d6ff916dd1d00fb4ae0389e7c6d678a
ChaiYihan <2366791707@qq.com>
2022-06-24 11:35:42 +0800

    change ico and title of web

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 8090,
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 })
